bd3b672279e40d98e804fa52b235310b15b96645
Bbz5 <marco.barbato2022@campus-eni.fr>
2022-07-20 21:35:32 +0200

    Creation de l'Entity Etat e de sa relation avec Sortie

---
+++ src/Form/SortieType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie'])
+            ->add('dateHeureDebut', DateType::class, [
+                'label'=> 'Date et heure de la sortie',  'html5'=> true, 'widget' => 'single_text'])
+            ->add('dateLimiteInscription', DateType::class, [
+                'label'=> "Date limite d'inscription", 'html5'=> true, 'widget'=> 'single_text'])
+            ->add('nbInscriptionMax', NumberType::class, [
+                'label' => 'Nombre de place'
+            ] )
+            ->add('duree', NumberType::class, [
+                'label'=>'Durée'
+                ]
+            )
+            ->add('infosSortie', TextareaType::class, [
+                'label'=> 'Description et infos'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
